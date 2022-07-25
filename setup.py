ebacbf75e33477ddb76b9301dacefb6476662694
charlie <imo.charlie@gmail.com>
2022-07-25 08:43:33 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='pymathlang',
+   version='0.0.1',
+   description='A wrapper of MathML using Python Objects',
+   author='Charlie Yu',
+   author_email='imo.charlie@gmail.com',
+)
